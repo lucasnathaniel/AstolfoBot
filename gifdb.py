22b60ff0875c510b0d4f00453c94d62b0921a8d8
Lucas Nathaniel <lucasnataniel@hotmail.com>
2020-07-25 14:01:44 -0300

    migrating files from hideri

---
+++ gifdb.py
@@ -0,0 +1,181 @@
+from random import choice
+
+def get_gif(type):
+	if type == "bite":
+		return choice([
+			"https://media1.tenor.com/images/c22a247affcf4cd02c7d17f5a432cd95/tenor.gif?itemid=8259627",
+			"https://media1.tenor.com/images/17f0fc8bc1e0d5df5f519b8cd9237ac8/tenor.gif?itemid=5384805",
+			"https://media1.tenor.com/images/7235a20d39ce65bfc1efd7f4a1cea1ee/tenor.gif?itemid=14340030",
+			"https://media.giphy.com/media/OqQOwXiCyJAmA/giphy.gif",
+			"https://media.giphy.com/media/3qa8z4JpTTbsQ/giphy.gif",
+			"https://i.giphy.com/media/rd5B86UjFkDEA/giphy.webp",
+			"https://media1.tenor.com/images/0d192209c8e9bcd9826af63ba72fc584/tenor.gif?itemid=15164408",
+			"https://i.gifer.com/MHg3.gif"
+			])
+	elif type == "greet":
+		return choice([
+			"https://media1.tenor.com/images/3cde3e1fe79e02abdc287395f57d8578/tenor.gif?itemid=16679443",
+			"https://media1.tenor.com/images/1c5b339df666dde2a03276e8da9c66bd/tenor.gif?itemid=12660748",
+			"https://media1.tenor.com/images/fcc3854ad5ee2c22eb0189998be4c8f8/tenor.gif?itemid=14835859",
+			"https://media1.tenor.com/images/056c584d9335fcabf080ca43e583e3c4/tenor.gif?itemid=8994845",
+			"https://media1.tenor.com/images/dbbacb1d705d2713ff57118d274e15c8/tenor.gif?itemid=15936743",
+			"https://media1.tenor.com/images/b5397d5803883e7d4a4b355d068a7375/tenor.gif?itemid=14223397",
+			"https://media1.tenor.com/images/a7bd6b94430c1e66148d580209e377c5/tenor.gif?itemid=5043108",
+			"https://media1.tenor.com/images/79f33c2f524cbfed4ef6896b39e67663/tenor.gif?itemid=9416181",
+			"https://media1.tenor.com/images/f5cd33863e8319ea72990eefc8e697a8/tenor.gif?itemid=5417197",
+			"https://media1.tenor.com/images/b5e7bcf6d57424371cca15e014c158ce/tenor.gif?itemid=15412719",
+			"https://media1.tenor.com/images/37aa92ebab4f4657a8f1b913b4ea7da4/tenor.gif?itemid=5970516",
+			"https://media1.tenor.com/images/2ef78ab2f3e2acbf077388e26d3bc2da/tenor.gif?itemid=14815980",
+			"https://media1.tenor.com/images/b82e6a78b221f7dc2e41605b6aa2cbcc/tenor.gif?itemid=11503720",
+			"https://media1.tenor.com/images/af87df7587d7c41691d464fdccdfba34/tenor.gif?itemid=5634612",
+			"https://media1.tenor.com/images/bf75ccc99dab1b7a74b31460ee33191b/tenor.gif?itemid=14070427"
+			])
+	elif type == "highfive":
+		return choice([
+			"https://media1.tenor.com/images/7b1f06eac73c36721912edcaacddf666/tenor.gif?itemid=10559431",
+			"https://media1.tenor.com/images/c3263b8196afc25ddc1d53a4224347cd/tenor.gif?itemid=9443275",
+			"https://media1.tenor.com/images/14226cfc51aeaf72be5ec5ac71b1155b/tenor.gif?itemid=16227553",
+			"https://media1.tenor.com/images/0c23b320822afd5b1ce3faf01c2b9b69/tenor.gif?itemid=14137078",
+			"https://media1.tenor.com/images/b714d7680f8b49d69b07bc2f1e052e72/tenor.gif?itemid=13400356",
+			"https://media1.tenor.com/images/30de2a0a913599f7770b1d3edba31f56/tenor.gif?itemid=7283511",
+			"https://media1.tenor.com/images/0b8c573239f032bb3156d35b0d5865d2/tenor.gif?itemid=13354957",
+			"https://media1.tenor.com/images/1b1597b6b5a36a1d3ac10047c030a22c/tenor.gif?itemid=16312860",
+			"https://media1.tenor.com/images/9730876547cb3939388cf79b8a641da9/tenor.gif?itemid=8073516",
+			"https://media1.tenor.com/images/ce85a2843f52309b85515f56a0a49d06/tenor.gif?itemid=14137077",
+			"https://media1.tenor.com/images/4ee86c5471b564c93a36cf1f99c5dbb8/tenor.gif?itemid=16129960",
+			"https://media1.tenor.com/images/0ae4995e4eb27e427454526c05b2e3dd/tenor.gif?itemid=12376992"
+			])
+	elif type == "hug":
+		return choice([
+			"https://media1.tenor.com/images/1069921ddcf38ff722125c8f65401c28/tenor.gif?itemid=11074788",
+			"https://media1.tenor.com/images/94989f6312726739893d41231942bb1b/tenor.gif?itemid=14106856",
+			"https://media1.tenor.com/images/506aa95bbb0a71351bcaa753eaa2a45c/tenor.gif?itemid=7552075",
+			"https://media1.tenor.com/images/af76e9a0652575b414251b6490509a36/tenor.gif?itemid=5640885",
+			"https://media1.tenor.com/images/4d89d7f963b41a416ec8a55230dab31b/tenor.gif?itemid=5166500",
+			"https://media1.tenor.com/images/e58eb2794ff1a12315665c28d5bc3f5e/tenor.gif?itemid=10195705",
+			"https://media1.tenor.com/images/460c80d4423b0ba75ed9592b05599592/tenor.gif?itemid=5044460",
+			"https://media1.tenor.com/images/49a21e182fcdfb3e96cc9d9421f8ee3f/tenor.gif?itemid=3532079",
+			"https://media1.tenor.com/images/42922e87b3ec288b11f59ba7f3cc6393/tenor.gif?itemid=5634630",
+			"https://media1.tenor.com/images/1a73e11ad8afd9b13c7f9f9bb5c9a834/tenor.gif?itemid=13366388",
+			"https://media1.tenor.com/images/b87f8b1e2732c534a00937ffb24baa79/tenor.gif?itemid=9136391"
+			])
+	elif type == "kiss":
+		return choice([
+			"https://i.giphy.com/media/G3va31oEEnIkM/giphy.webp",
+			"https://i.giphy.com/media/hnNyVPIXgLdle/giphy.webp",
+			"https://i.giphy.com/media/nyGFcsP0kAobm/giphy.webp",
+			"https://media.giphy.com/media/zkppEMFvRX5FC/giphy.gif",
+			"https://i.giphy.com/media/mGAzm47irxEpG/giphy.webp",
+			"https://i.giphy.com/media/JynbO9pnGxPrO/giphy.webp",
+			"https://media1.tenor.com/images/279c4716a469ace39b15e34d7fa3e7c4/tenor.gif?itemid=11487318",
+			"https://media1.tenor.com/images/ef4a0bcb6e42189dc12ee55e0d479c54/tenor.gif?itemid=12143127",
+			"https://media1.tenor.com/images/621ceac89636fc46ecaf81824f9fee0e/tenor.gif?itemid=4958649",
+			"https://media1.tenor.com/images/ea9a07318bd8400fbfbd658e9f5ecd5d/tenor.gif?itemid=12612515",
+			"https://media1.tenor.com/images/ba1841e4aeb5328e41530d3289616f46/tenor.gif?itemid=14240425",
+			"https://media1.tenor.com/images/b8d0152fbe9ecc061f9ad7ff74533396/tenor.gif?itemid=5372258",
+			"https://media1.tenor.com/images/d5a3f43f9b2ed8900834607d59916a9c/tenor.gif?itemid=13516822",
+			"https://media1.tenor.com/images/7fd98defeb5fd901afe6ace0dffce96e/tenor.gif?itemid=9670722",
+			"https://media1.tenor.com/images/68a37a5a1b86f227b8e1169f33a6a6bb/tenor.gif?itemid=13344389",
+			"https://media1.tenor.com/images/558f63303a303abfdddaa71dc7b3d6ae/tenor.gif?itemid=12879850",
+			"https://media1.tenor.com/images/40711a5b00fcf9918ddef1aa483d993f/tenor.gif?itemid=11737410",
+			"https://media1.tenor.com/images/ad514e809adc14f0b7722a324c2eb36e/tenor.gif?itemid=14375355"
+			])
+	elif type == "lewd":
+		return choice([
+			"https://media1.tenor.com/images/023c6e776cb2191f82afdaa7c012bfc5/tenor.gif?itemid=12370247",
+			"https://media1.tenor.com/images/7675abb5695ab508f7a4bb64e354a942/tenor.gif?itemid=14953446",
+			"https://media1.tenor.com/images/c4bad2fc648dc7b00e9e1216a749dc90/tenor.gif?itemid=15487135",
+			"https://media1.tenor.com/images/3160ca92ab4b8b010c88e53b6461e771/tenor.gif?itemid=7407062",
+			"https://media1.tenor.com/images/b43df533b92a60a6302a3fceb6ea532c/tenor.gif?itemid=4841555",
+			"https://media1.tenor.com/images/afa7dcadb195d30e22da445146f9441f/tenor.gif?itemid=16038421",
+			"https://media1.tenor.com/images/bc2810fc980244dfe6b3f0993eb70486/tenor.gif?itemid=13984951",
+			"https://media1.tenor.com/images/3657bb26403858e3a2051e9aeeb540dd/tenor.gif?itemid=8979689",
+			"https://cdn.discordapp.com/attachments/631194465452687393/693239541066366997/lewd-rynf6aOPb..png",
+			"https://cdn.discordapp.com/attachments/631194465452687393/693240194127888454/lewd-SyeBQpTdDW..jpeg",
+			"https://cdn.discordapp.com/attachments/631194465452687393/693239426578513950/lewd-BJGHapdvW..gif"
+			])
+	elif type == "cuddle":
+		return choice([
+			"https://i.imgur.com/EVBa1rR.gif",
+			"https://i.imgur.com/iUiDUGC.gif",
+			"https://i.imgur.com/ngGm9v9.gif",
+			"https://i.imgur.com/G2DLX6i.gif",
+			"https://i.imgur.com/r8BRNYz.gif",
+			"https://i.imgur.com/9uHap1W.gif",
+			"https://i.imgur.com/UP9CmgY.gif",
+			"https://i.imgur.com/V79eI.gif",
+			"https://i.imgur.com/KrYnQzR.gif",
+			"https://media1.tenor.com/images/218aed7cb7c6b37183cff6baed7166b8/tenor.gif?itemid=16736544",
+			"https://media1.tenor.com/images/3d62384321435408f50823ae6f5ca033/tenor.gif?itemid=12270770",
+			"https://media1.tenor.com/images/d0c2e7382742f1faf8fcb44db268615f/tenor.gif?itemid=5853736",
+			"https://media1.tenor.com/images/9af57b60dca6860724a0ff6c1689c246/tenor.gif?itemid=8467962",
+			"https://media1.tenor.com/images/4179058caa9eef3e7c6b21b8888b9cc9/tenor.gif?itemid=12955956",
+			"https://media1.tenor.com/images/08de7ad3dcac4e10d27b2c203841a99f/tenor.gif?itemid=4885268",
+			"https://media1.tenor.com/images/8f8ba3baeecdf28f3e0fa7d4ce1a8586/tenor.gif?itemid=12668750",
+			"https://media1.tenor.com/images/13be52a4a4a26b0c9e479df6644d6de5/tenor.gif?itemid=12668752",
+			"https://media1.tenor.com/images/bd8d54e9127ee5c43ecc9503858f0f49/tenor.gif?itemid=15759500",
+			"https://media1.tenor.com/images/3c295a01cf438f45d07ec5f8b1f9dd4e/tenor.gif?itemid=12668760",
+
+		])
+	elif type == "pat":
+		return choice([
+			"https://media1.tenor.com/images/16ec668d63ac6c1e8a1635b855cb1654/tenor.gif?itemid=14623443",
+			"https://media1.tenor.com/images/f5176d4c5cbb776e85af5dcc5eea59be/tenor.gif?itemid=5081286",
+			"https://media1.tenor.com/images/da8f0e8dd1a7f7db5298bda9cc648a9a/tenor.gif?itemid=12018819",
+			"https://media1.tenor.com/images/f330c520a8dfa461130a799faca13c7e/tenor.gif?itemid=13911345",
+			"https://media1.tenor.com/images/116fe7ede5b7976920fac3bf8067d42b/tenor.gif?itemid=9200932",
+			"https://media1.tenor.com/images/0d2fb6ad9a6d71c3a018c0b37ffca50b/tenor.gif?itemid=16121044",
+			"https://media1.tenor.com/images/54722063c802bac30d928db3bf3cc3b4/tenor.gif?itemid=8841561",
+			"https://media1.tenor.com/images/005e8df693c0f59e442b0bf95c22d0f5/tenor.gif?itemid=10947495",
+			"https://media1.tenor.com/images/183ff4514cbe90609e3f286adaa3d0b4/tenor.gif?itemid=5518321",
+			"https://media1.tenor.com/images/1e92c03121c0bd6688d17eef8d275ea7/tenor.gif?itemid=9920853",
+			"https://media1.tenor.com/images/bfdeb9ec7f89aad86170d06fe4189bec/tenor.gif?itemid=16085314",
+			"https://media1.tenor.com/images/71e74263a48a6e9a2c53f3bc1439c3ac/tenor.gif?itemid=12434286",
+			"https://media1.tenor.com/images/755b519f860ef65a4b9f889aece000fe/tenor.gif?itemid=16085284",
+			"https://media1.tenor.com/images/70960e87fb9454df6a1d15c96c9ad955/tenor.gif?itemid=10092582",
+			"https://media1.tenor.com/images/078599227bc087959b79ea111fbc0f3a/tenor.gif?itemid=13596135"
+			])
+	elif type == "poke":
+		return choice([
+			"https://media1.tenor.com/images/2b55eb1befce3e843dec7e8feebf274b/tenor.gif?itemid=10168199",
+			"https://media1.tenor.com/images/01b264dc057eff2d0ee6869e9ed514c1/tenor.gif?itemid=14346763",
+			"https://media1.tenor.com/images/e8b25e7d069c203ea7f01989f2a0af59/tenor.gif?itemid=12011027",
+			"https://media1.tenor.com/images/514efe749cb611eb382713596e3427d8/tenor.gif?itemid=13054528",
+			"https://media1.tenor.com/images/cbf38a2e97a348a621207c967a77628a/tenor.gif?itemid=6287077",
+			"https://media1.tenor.com/images/ab936c887562756472f83850426bf6ef/tenor.gif?itemid=11956062",
+			"https://media1.tenor.com/images/d2b08ce502740221b978d8e5e876b6e2/tenor.gif?itemid=12872012",
+			"https://media1.tenor.com/images/3b2bfd09965bd77f2a8cb9ba59cedbe4/tenor.gif?itemid=5607667",
+			"https://media1.tenor.com/images/faa16497a7cd01dc7c947225b27062f5/tenor.gif?itemid=12583170",
+			"https://media1.tenor.com/images/d9b55173939b863da320ddba91e13b91/tenor.gif?itemid=15148498"
+			])
+	elif type == "slap":
+		return choice([
+			"https://media1.tenor.com/images/d14969a21a96ec46f61770c50fccf24f/tenor.gif?itemid=5509136",
+			"https://media1.tenor.com/images/3fd96f4dcba48de453f2ab3acd657b53/tenor.gif?itemid=14358509",
+			"https://media1.tenor.com/images/89309d227081132425e5931fbbd7f59b/tenor.gif?itemid=4880762",
+			"https://media1.tenor.com/images/9ea4fb41d066737c0e3f2d626c13f230/tenor.gif?itemid=7355956",
+			"https://media1.tenor.com/images/1ba1ea1786f0b03912b1c9138dac707c/tenor.gif?itemid=5738394",
+			"https://media1.tenor.com/images/53d180f129f51575a46b6d3f0f5eeeea/tenor.gif?itemid=5373994",
+			"https://media1.tenor.com/images/4a6b15b8d111255c77da57c735c79b44/tenor.gif?itemid=10937039",
+			"https://media1.tenor.com/images/477821d58203a6786abea01d8cf1030e/tenor.gif?itemid=7958720",
+			"https://media1.tenor.com/images/358986720d4b533a49bdb67cbc4fe3e5/tenor.gif?itemid=14179582",
+			"https://media1.tenor.com/images/b221fb3f50f0e15b3ace6a2b87ad0ffa/tenor.gif?itemid=8576304",
+			"https://media1.tenor.com/images/fb17a25b86d80e55ceb5153f08e79385/tenor.gif?itemid=7919028",
+			"https://media1.tenor.com/images/7437caf9fb0bea289a5bb163b90163c7/tenor.gif?itemid=13595529",
+			"https://media1.tenor.com/images/b6d8a83eb652a30b95e87cf96a21e007/tenor.gif?itemid=10426943",
+			"https://media1.tenor.com/images/c159cd1d7e7424cf9fd6fbdb09919146/tenor.gif?itemid=14179570",
+			"https://media1.tenor.com/images/dcd359a74e32bca7197de46a58ec7b72/tenor.gif?itemid=12396060",
+			"https://media1.tenor.com/images/0860d681fbe7ad04a2f39735ab939176/tenor.gif?itemid=13642334"
+			])
+	elif type == "lick":
+		return choice([
+			"https://media1.tenor.com/images/dbc120cf518319ffe2aedf635ad2df93/tenor.gif?itemid=16600144",
+			"https://media1.tenor.com/images/5f73f2a7b302a3800b3613095f8a5c40/tenor.gif?itemid=10005495",
+			"https://media1.tenor.com/images/f46762ad38fbfed9e4e46bf7b89497c2/tenor.gif?itemid=12141724",
+			"https://media1.tenor.com/images/5b94662a631d4276cf135b274f0ce9af/tenor.gif?itemid=14848171",
+			"https://media1.tenor.com/images/b00d152c5645975a06c4916e360635ef/tenor.gif?itemid=15900643",
+			"https://media1.tenor.com/images/5c5828e51733c8ffe1c368f1395a03d0/tenor.gif?itemid=14231351",
+			"https://media1.tenor.com/images/c1d9ff6f013a3deba4b7941fa00374e5/tenor.gif?itemid=16150817",
+			"https://media1.tenor.com/images/efd46743771a78e493e66b5d26cd2af1/tenor.gif?itemid=14002773",
+			"https://media1.tenor.com/images/797fcf0d3bf9515565f554ebdfb43e7c/tenor.gif?itemid=16414426"
+			])
